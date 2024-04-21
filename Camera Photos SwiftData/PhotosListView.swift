e7ed510d9c30cd573db4413f5970ab412c3acdba
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2024-04-21 22:52:35 +0200

    feat: PhotosListView

--- Camera Photos SwiftData/PhotosListView.swift
+++ Camera Photos SwiftData/PhotosListView.swift
@@ -6,19 +6,58 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct PhotosListView: View {
+    @Query(sort: \SampleModel.name) var samples: [SampleModel]
+    @Environment(\.modelContext) private var modelContext
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Group {
+                if samples.isEmpty {
+                    ContentUnavailableView("Add your first photo", systemImage: "photo")
+                } else {
+                    List(samples) { sample in
+                        NavigationLink(value: sample) {
+                            HStack {
+                                Image(uiImage: sample.image == nil ? Constants.placeholder : sample.image!)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 50, height: 50)
+                                    .cornerRadius(12)
+                                    .clipped()
+                                    .padding(.trailing)
+                                Text(sample.name)
+                                    .font(.title)
+                            }
+                        }
+                        .swipeActions {
+                            Button(role: .destructive) {
+                                modelContext.delete(sample)
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }
+                    }
+                    .listStyle(.plain)
+                }
+            }
+            .navigationDestination(for: SampleModel.self) { sample in
+                
+            }
+            .navigationTitle("Picker or Camera")
+            .toolbar {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     PhotosListView()
+        .modelContainer(SampleModel.preview)
 }
