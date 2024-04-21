eac719a3e79a1465d318334e8ce40fc28d692699
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2024-04-21 22:01:05 +0200

    first commit: Load initial files

---
+++ Camera Photos SwiftData/Camera_Photos_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_Photos_SwiftDataApp.swift
+//  Camera Photos SwiftData
+//
+//  Created by Marcin Jędrzejak on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Camera_Photos_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Camera Photos SwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Camera Photos SwiftData
+//
+//  Created by Marcin Jędrzejak on 21/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
