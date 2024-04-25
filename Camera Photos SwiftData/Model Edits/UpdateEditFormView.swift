2dd6a382a11c7c807bea9c71ec6aab95f9ed7469
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2024-04-25 23:07:52 +0200

    feat: PhotosUI and PhotosPicker

--- Camera Photos SwiftData/Model Edits/UpdateEditFormView.swift
+++ Camera Photos SwiftData/Model Edits/UpdateEditFormView.swift
@@ -7,12 +7,14 @@
 
 import SwiftUI
 import SwiftData
+import PhotosUI
 
 struct UpdateEditFormView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) private var modelContext
     @State var vm: UpdateEditFormViewModel
-
+    @State private var imagePicker = ImagePicker()
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -28,9 +30,55 @@ struct UpdateEditFormView: View {
                         Button("Camera", systemImage: "camera") {
                             
                         }
+                        PhotosPicker(selection: $imagePicker.imageSelection) {
+                            Label("Photos", systemImage: "photo")
+                        }
                     }
                     .foregroundStyle(.white)
                     .buttonStyle(.borderedProminent)
+                    Image(uiImage: vm.image)
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .padding()
+                }
+            }
+            .onAppear {
+                imagePicker.setup(vm)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        if vm.isUpdating {
+                            if let sample = vm.sample {
+                                if vm.image != Constants.placeholder {
+                                    sample.data = vm.image.jpegData(compressionQuality: 0.8)
+                                } else {
+                                    sample.data = nil
+                                }
+                                sample.name = vm.name
+                                dismiss()
+                            }
+                        } else {
+                            let newSample = SampleModel(name: vm.name)
+                            if vm.image != Constants.placeholder {
+                                newSample.data = vm.image.jpegData(compressionQuality: 0.8)
+                            } else {
+                                newSample.data = nil
+                            }
+                            modelContext.insert(newSample)
+                            dismiss()
+                        }
+                        
+                    } label: {
+                        Text(vm.isUpdating ? "Update" : "Add")
+                    }
+                    .disabled(vm.isDisabled)
                 }
             }
         }
